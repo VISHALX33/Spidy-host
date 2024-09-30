309832472a81f5abd76d5da61e8efbfb37ab27a4
VISHALX33 <vishalprajapati2303@gmail.com>
2024-09-30 17:15:17 +0530

    complete the Testimonial section

--- my-react-app/src/Componentes/PremierControl.jsx
+++ my-react-app/src/Componentes/PremierControl.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 function PremierControl() {
   return (
-    <div>PremierControl</div>
+    // <div className='flex flex-col'>
+      <div className=' text-center '>
+      Premier Control Panel
+      </div>
+    //   <p className=' text-center '>cPanel stands out as a top-notch and user-friendly tool for managing web hosting. It makes handling your websites and hosting account a breeze. With cPanel, you can easily set up and manage your files, databases, email accounts, SSL certificates, and more—all in one convenient place. It simplifies the management of your hosting account, providing a user-friendly experience.</p>
+    // </div>
   )
 }
 
--- my-react-app/src/Componentes/Testimonial.jsx
+++ my-react-app/src/Componentes/Testimonial.jsx
@@ -1,8 +1,41 @@
 import React from 'react'
+import trustpilot from '../assets/ControlPanel/trustpilot.webp'
+
 
 function Testimonial() {
   return (
-    <div>Testimonial</div>
+    <div className='bg-white py-10'>
+      <h1 className='text-center text-4xl mt-20'>
+        See what our customers think about us...
+      </h1>
+      <div className='grid md:grid-cols-1 lg:grid-cols-3  xl:grid-cols-3 sm:grid-cols-2 grid-cols-1 mb-5 gap-6 text-start mt-16  mx-32  '>
+        <div  className='border-gray-300 border-2 p-5   '>
+        <img src={trustpilot} alt="" className='py-5' />
+        <div style={{ fontSize: '24px', color: 'purple', marginBottom: '20px' }}>
+      ★ ★ ★ ★ ★
+    </div>
+          <p className='text-gray-800 '>Server is very fast and most important thing is service, support team given very quick response and good service.</p>
+          <p className='mt-12 font-bold'>Satendra</p>
+        </div>
+
+        <div  className='border-gray-300 border-2 p-5  '>
+        <img src={trustpilot} alt="" className='py-5'/>
+        <div style={{ fontSize: '24px', color: 'purple', marginBottom: '20px' }}>
+      ★ ★ ★ ★ ★
+    </div>
+          <p className='text-gray-800'>Spidyhost.com has Mr.Shaik Vahid such amazing great Tech support that will take care of all your technical problems no matter how difficult it was.</p>
+          <p className='mt-12 font-bold'>Huo Yeh</p>
+        </div>
+        <div  className='border-gray-300 border-2 p-5  '>
+        <img src={trustpilot} alt="" className='py-5' />
+        <div style={{ fontSize: '24px', color: 'purple', marginBottom: '20px' }}>
+      ★ ★ ★ ★ ★
+    </div>
+          <p className='text-gray-800'>I totally love this hosting service, The Spidy Host team is the best. They are always ready to assist. I highly recommend this service for any business type.</p>
+          <p className='mt-12 font-bold'>Digital Lync</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
